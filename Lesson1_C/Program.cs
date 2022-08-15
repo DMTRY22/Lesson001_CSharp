1a5142b1a73b0c85d4f9141b16c58c2a640cc3e1
Bogatyrev Dmitry <bogatyrev.dm@mail.ru>
2022-08-15 17:03:35 +0300

    добавленно новое дз

--- Lesson1_C/Program.cs
+++ Lesson1_C/Program.cs
@@ -69,3 +69,13 @@
 // i++;
 
 // Console.WriteLine();
+
+
+// ДЗ по второй лекции
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int secondNumber = num / 10 % 10;
+Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
