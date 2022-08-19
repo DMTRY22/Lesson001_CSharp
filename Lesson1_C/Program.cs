fc7b122321fa087d3d6c1ce9edc1bd4cf48f19d3
Bogatyrev Dmitry <bogatyrev.dm@mail.ru>
2022-08-19 22:50:48 +0300

    добавленно новое дз

--- Lesson1_C/Program.cs
+++ Lesson1_C/Program.cs
@@ -145,25 +145,25 @@
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-void distance(double a, double b, double c, double d, double e, double f);
-{
-    Console.WriteLine($"расстояние между точками равно {Math.Round(Math.Sqrt(Math.Pow(a - d, 2) + Math.Pow(b - e, 2) + Math.Pow(c - f, 2)), 2)}");
+// void distance(double a, double b, double c, double d, double e, double f);
+// {
+//     Console.WriteLine($"расстояние между точками равно {Math.Round(Math.Sqrt(Math.Pow(a - d, 2) + Math.Pow(b - e, 2) + Math.Pow(c - f, 2)), 2)}");
 
-}
-Console.WriteLine("введите координаты двух точек в 3D");
+// }
+// Console.WriteLine("введите координаты двух точек в 3D");
 
-Console.WriteLine("x первой точки");
-double xa = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("y первой точки");
-double ya = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("z первой точки");
-double za = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("x первой точки");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("y первой точки");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("z первой точки");
+// double za = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("x первой точки");
-double xb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("y первой точки");
-double xb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("z первой точки");
-double xb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("x первой точки");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("y первой точки");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("z первой точки");
+// double xb = Convert.ToDouble(Console.ReadLine());
 
-distance(xa, ya, za, xb, yb, zb);
+// distance(xa, ya, za, xb, yb, zb);
