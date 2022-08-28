4b43a45455f15995e1eecfd753a57b3668191496
Bogatyrev Dmitry <bogatyrev.dm@mail.ru>
2022-08-28 14:58:21 +0300

    добавленно новое дз

--- Lesson1_C/Program.cs
+++ Lesson1_C/Program.cs
@@ -208,3 +208,158 @@
 //     Console.WriteLine($"{sum}");
 // }
 // sumDigits(numbers);
+
+
+
+// ДЗ по пятой лекции.
+
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Наш массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int z = 0; z < numbers.Length; z++)
+// if (numbers[z] % 2 == 0)
+// count++;
+
+// Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// Console.Clear();
+// Console.WriteLine($" Сумма элементов, стоящих на нечётных позициях:");
+// Console.Write($" Введи количество элементов массива:");
+// int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+// int RandomNumbers(int numberElements, int min, int max)
+//   {
+//   int[] randomNumbers = new int[numberElements];
+//   int sumElements = 0;
+//   Console.Write("массив: ");
+
+//     for (int i = 0; i <randomNumbers.Length; i++ ){
+//       randomNumbers[i] = new Random().Next(min, max);
+
+//       Console.Write(randomNumbers[i] + " ");
+
+//       if (i % 2 != 1)
+//       {
+//         sumElements = sumElements + randomNumbers[i];
+//       }
+//     }
+//   return sumElements;
+//   }
+
+// int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+// Console.WriteLine($" Сумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// Console.WriteLine($" Задайте массив вещественных чисел:");
+
+// double[] arrayRealNumbers = new double[10];
+//   for (int i = 0; i < arrayRealNumbers.Length; i++ )
+//   {
+//     arrayRealNumbers[i] = new Random().Next(1, 10);
+//     Console.Write(arrayRealNumbers[i] + " ");
+//   }
+
+// double maxNumber = arrayRealNumbers[0];
+// double minNumber = arrayRealNumbers[0];
+
+//   for (int i = 1; i < arrayRealNumbers.Length; i++)
+//   {
+//     if (maxNumber < arrayRealNumbers[i])
+//     {
+//       maxNumber = arrayRealNumbers[i];
+//     }
+//         if (minNumber > arrayRealNumbers[i])
+//     {
+//       minNumber = arrayRealNumbers[i];
+//     }
+//   }
+
+//   double decision = maxNumber - minNumber;
+
+//   Console.WriteLine($"({maxNumber}) ({minNumber}) {decision}");
+
+
+// ДЗ по шестой лекции.
+
+
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Console.Clear();
+// Console.Write("ввести числа через пробел: ");
+// Console.WriteLine($"Количество чисел больше ноля 0 -> {GetCountPositiveElements(numbers)}");
+// Console.WriteLine();
+
+// int[] GetArrayFromString(string stringArray)
+// {
+// string[] numS = stringArray.Split('', StringSplitOptions.RemoveEmptyEntries);
+// int[] result = new int[numS.Length];
+// for (int i = 0; i < result.Length; i++)
+// {
+//     result[i] = int.Parse(numS[i]);
+// }
+// return result;
+// }
+
+// int GetCountPositiveElements(int[] array)
+// {
+//     int count = 0;
+//     foreach (var item in array)
+//     {
+//         if(item>0) count++;
+//     }
+//     return count;
+// }
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Console.Clear();
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+// var x = -(b1 - b2) / (k1 - k2);
+// var y = k1 * x + b1;
+ 
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+ 
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
